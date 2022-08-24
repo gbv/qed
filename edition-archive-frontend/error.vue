ca6f95b15dd652ef5f06c19ae710e8f1ed796b1a
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-08-24 10:10:42 +0200

    Added basic error handling

---
+++ edition-archive-frontend/error.vue
@@ -0,0 +1,68 @@
+<template>
+  <NuxtLayout>
+    <div class="error-info-hidden">
+      <!-- typeof error.statusCode: -->
+      <!-- string from server-side -->
+      <!-- number on client-side -->
+      typeof error.statusCode =
+      <span v-html="`${typeof error.statusCode}`"> </span>
+      <hr/>
+
+      <p>
+        error.statusCode: {{ error.statusCode }} <br/>
+        error.message: {{ error.message }}
+      </p>
+      <hr/>
+
+    </div>
+    <!-- customise 404 message from template section -->
+
+    <div class="wrapper">
+      <section class="error 404" v-if="error.statusCode === 404 || '404'">
+        <h3>{{ $t("error_404") }}</h3>
+        <!-- {{ $t("error_404") }}-->
+        <button class="btn btn-primary float-end" @click="handleError">{{ $t("back") }}</button>
+      </section>
+      <section class="error" v-else>
+        <h3>{{ $t("error") }}</h3>
+      </section>
+    </div>
+
+
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+// default props available on error.vue
+const props = defineProps({
+  error: Object,
+});
+
+// customise 404 message from script section
+const error = useError();
+const route = useRoute();
+
+// clear error and redirect to home page
+const handleError = () => {
+  const path = route.path;
+  const urlParts = path.split("/");
+  const redirect = urlParts.length < 2 ? "/" : "/" + urlParts[0];
+  console.log(urlParts);
+  console.log("TO: " + redirect);
+  clearError({redirect: redirect});
+}
+</script>
+
+<style scoped>
+
+.error-info-hidden {
+  display: none;
+}
+
+.error {
+  text-align: center;
+}
+
+.wrapper {
+}
+</style>
