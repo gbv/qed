f86a8bcfc3ffa53e07748a4ee6f5795400b9e185
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2023-04-17 17:08:36 +0200

    QUED-67 add abstract

--- edition-archive-frontend/api/Utils.ts
+++ edition-archive-frontend/api/Utils.ts
@@ -4,19 +4,47 @@ export function pad(num: number, size: number) {
   return numStr;
 }
 
-export function trimString(str: string | null): string | null {
+function* trimStringTokens(abstract: string) {
+  const regex = /(?<content>[^, .!?]+)(?<seperator>[, .!?]+)/g;
+
+  let m: any;
+  while ((m = regex.exec(abstract)) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    yield {content: m?.groups?.content, sep: m?.groups?.seperator}
+  }
+}
+
+export function trimString(str: string | null, maxLength = 200): string | null {
   if (str == null) {
     return null;
   }
-  const size = 240;
+  let tokens = trimStringTokens(str);
+  let count = 0;
+  let next;
+  let result = "";
+  let lastSep = "";
+  while ((next = tokens.next().value) != null) {
+    const {content, sep} = next;
+    count += content.length + sep.length;
 
-  if (str.length > size) {
-    const trimmed = str.substring(0, size).trim();
-    const lastSpaceIndex = trimmed.lastIndexOf(' ');
-    return lastSpaceIndex === -1 ? trimmed + "…" : trimmed.substring(0, lastSpaceIndex) + "…";
-  } else {
-    return str;
+    if (count > maxLength) {
+      if(result.length === 0){
+        // the first token is already too long. Just return the first 200 characters
+        return str.substring(0, maxLength);
+      } else {
+        return result + "…";
+      }
+    }
+    // the text continues to fit. Add the token to the result
+    result += lastSep;
+    result += content;
+
+    // remember the last separator, so we can add it to the next token.
+    lastSep = sep;
   }
+  return result;
 }
 
 export function escapeSpecialChars(s: string) {
