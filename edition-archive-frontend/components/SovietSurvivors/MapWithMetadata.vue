e3d918b1fec3a8379d67993be83e905b1d9eb2d8
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2025-07-08 19:13:46 +0200

    QUED-160 Update map functionality with clustering, overlays, and metadata integration

--- edition-archive-frontend/components/SovietSurvivors/MapWithMetadata.vue
+++ edition-archive-frontend/components/SovietSurvivors/MapWithMetadata.vue
@@ -6,7 +6,7 @@
 
     <ol-view
       ref="view"
-      :center="transform([props.centerX, props.centerY], 'EPSG:4326', 'EPSG:3857')"
+      :center="model.position"
       :rotation="model.rotation"
       :zoom="model.zoom"
       :projection="model.projection"
@@ -17,20 +17,36 @@
     </ol-tile-layer>
 
     <ol-vector-layer>
-      <ol-source-vector :features="convertedSolrDocument">
-      </ol-source-vector>
-      <ol-style>
-        <ol-style-icon :src="markerIcon" :scale="0.5"></ol-style-icon>
-      </ol-style>
+      <ol-source-cluster :distance="20">
+        <ol-source-vector :features="convertedSolrDocument">
+        </ol-source-vector>
+      </ol-source-cluster>
+
+      <ol-style :overrideStyleFunction="styleCluster"/>
+
     </ol-vector-layer>
 
     <ol-interaction-select
-    @select="selected"
-    >
-      <ol-style>
-        <ol-style-icon :src="markerIconSelected" :scale="1"></ol-style-icon>
-      </ol-style>
+      :spiral="false"
+      :animate="false"
+      :autoclose="true"
+      @select="selected">
+
+
+      <ol-style :overrideStyleFunction="styleSelectedCluster" />
+
     </ol-interaction-select>
+
+    <ol-overlay
+      v-if="model.selectedFeatures.length > 0"
+      :position="[model.overlayPositionX, model.overlayPositionY, 500, 500]"
+      :autoPan="true"
+    >
+      <div class="overlay-content">
+       <slot name="metadata" :solrdocs="selectedProperties"></slot>
+      </div>
+    </ol-overlay>
+
 
   </ol-map>
 </template>
@@ -43,23 +59,37 @@ import  {WKT} from 'ol/format';
 
 import markerIcon from "assets/map_pin_red.svg";
 import markerIconSelected from "assets/map_pin_dark_red.svg";
-import Feature from "ol/Feature";
+import Feature, {type FeatureLike} from "ol/Feature";
 import {Select} from "ol/interaction";
 import type {SelectEvent} from "ol/interaction/Select";
+import {Style, Circle, Stroke, Fill, Text} from "ol/style";
 
-const emit = defineEmits({
-  'objectSelected': (feature: any|null) => true
-});
+
+import {Cluster} from 'ol/source';
+import type GeometryCollection from "ol/geom/GeometryCollection";
+import {Point} from "ol/geom";
+
 
 const props = defineProps<{
   // hier muss das solr response als json rein welches, die dokumente mit dem Feld common.mods.coordinates mit WKT enthält
   solrResponse: any,
   centerX: number,
-  centerY: number
+  centerY: number,
+}>();
+
+const slots = defineSlots<{
+    metadata: (scope: { solrdocs: any[] }) => any
 }>()
 
 const wktField = "common.mods.coordinates";
 const wkt = new WKT();
+
+const selectedProperties = computed(() => {
+  return model.selectedFeatures.map((feature: Feature) => {
+    return feature.getProperties().properties;
+  });
+})
+
 
 const convertedSolrDocument = computed(() => {
   if(!props.solrResponse || props.solrResponse.response === undefined || props.solrResponse.response.numFound == 0) {
@@ -73,13 +103,20 @@ const convertedSolrDocument = computed(() => {
     }
 
     for (const coord of solrDoc[wktField]) {
-      const geometry = wkt.readGeometry(coord, {
+      let geometry = wkt.readGeometry(coord, {
         dataProjection: 'EPSG:4326',
         featureProjection: 'EPSG:3857'
       });
 
       if(!geometry) {
         continue;
+      }
+
+      if (geometry.getType() === "GeometryCollection") {
+        const geometryCollection = geometry as GeometryCollection;
+        if (geometryCollection.getGeometries().length > 0) {
+          geometry = geometryCollection.getGeometries()[0];
+        }
       }
 
       const feature = new Feature({
@@ -95,28 +132,83 @@ const convertedSolrDocument = computed(() => {
 });
 
 
+const styleCluster = (feature: FeatureLike, currentStyle: Style, resolution: number) => {
+  if (feature.get("features") === undefined) {
+    return currentStyle;
+  }
+
+  const clusterSize = feature.get("features").length;
+
+  currentStyle.setText(new Text({
+    text: clusterSize.toString(),
+    fill: new Fill({
+      color: '#fff'
+    })
+  }));
+
+  currentStyle.setImage(new Circle({
+    radius: 10,
+    stroke: new Stroke({
+      color: '#fff',
+    }),
+    fill: new Fill({
+      color: '#265E78'
+    })
+  }));
+
+
+  return currentStyle;
+};
+
+const styleSelectedCluster = (feature: FeatureLike, currentStyle: Style, resolution: number) => {
+  currentStyle.setText(new Text({
+    text: feature.get("features").length.toString(),
+    fill: new Fill({
+      color: '#fff'
+    }),
+  }));
+  currentStyle.setImage(new Circle({
+    radius: 15,
+    stroke: new Stroke({
+      color: '#fff',
+      width: 3,
+    }),
+    fill: new Fill({
+      color: '#265E78'
+    })
+  }));
+  return currentStyle;
+}
 
 const model = reactive({
   projection: "EPSG:3857",
   zoom: 5,
-  rotation: 0
+  rotation: 0,
+  overlayPositionX: 0,
+  overlayPositionY: 0,
+  selectedFeatures: [] as any[],
+  position: transform([props.centerX, props.centerY], 'EPSG:4326', 'EPSG:3857'),
 });
 
 const selected = (selectEvent: SelectEvent) => {
+  selectEvent.preventDefault();
   if(selectEvent.deselected !== undefined) {
-    emit('objectSelected', null);
+    model.selectedFeatures = [];
   }
   if(selectEvent.type=== 'select') {
     if(selectEvent.selected !== undefined && selectEvent.selected.length > 0) {
-      emit('objectSelected', selectEvent.selected[0].getProperties().properties);
+      model.selectedFeatures = selectEvent.selected[0].get("features");
+      let geometry = model.selectedFeatures[0].getGeometry();
+      if(geometry?.getType()=="Point") {
+        model.overlayPositionX = (geometry as Point).getCoordinates()[0];
+        model.overlayPositionY = (geometry as Point).getCoordinates()[1];
+      }
       return;
     }
   }
 
 
 };
-
-
 
 </script>
 
--- edition-archive-frontend/pages/soviet-survivors/[[sosuPath]].vue
+++ edition-archive-frontend/pages/soviet-survivors/[[sosuPath]].vue
@@ -5,46 +5,55 @@
       <CMSPage :slug="$route.path"/>
 
       <div v-if="$route.path=='/soviet-survivors/' && solrResponse!=null" class="row">
-        <div class="col-8">
+        <div class="col-12">
           <client-only>
             <SovietSurvivorsMapWithMetadata :center-x="20.019309"
                                             :center-y="52.114829"
                                             :solr-response="solrResponse"
                                             v-on:object-selected="objectSelected"
-            />
-          </client-only>
-        </div>
-        <div v-if="selectedObject != null" class="selected-object col-4">
-          <div v-if="selectedObject['derCount'] > 0" class="row">
-            <div class="col-12 text-center">
-              <div class="doc_thumbnail">
-                <img
-                  :src=" $sovietSurviorsURL() + '/api/iiif/image/v2/thumbnail/' + selectedObject['id'] + '/full/!300,300/0/default.jpg'"
-                  alt="thumbnail"
-                  class="img-thumbnail">
-              </div>
-            </div>
-          </div>
-          <div class="row mt-1">
-            <div class="col-12">
-              <h3>
-                <nuxt-link
-                  :to="hitLink(selectedObject)"
-                  class="main-title">
-                  {{ selectedObject["mods.title.main"] }}
-                </nuxt-link>
-              </h3>
+            >
+              <template #metadata="{ solrdocs }">
+                <div class="card documents_card m-3">
+                  <div class="card-body">
+                    <div class="row document mt-5" v-for="selectedObject in solrdocs">
 
-              <div class="abstract">
-                <div v-if="selectedObject['mods.abstract']?.length>0">
-                  {{ trimString(selectedObject["mods.abstract"][0]) }}
+                      <div v-if="selectedObject['derCount'] > 0" class="row">
+                        <div class="col-12 text-center">
+                          <div class="doc_thumbnail">
+                            <img
+                              :src=" $sovietSurviorsURL() + '/api/iiif/image/v2/thumbnail/' + selectedObject['id'] + '/full/!300,300/0/default.jpg'"
+                              alt="thumbnail"
+                              class="img-thumbnail">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mt-1">
+                        <div class="col-12">
+                          <h3 class="text-center">
+                            <nuxt-link
+                              :to="hitLink(selectedObject)"
+                              class="main-title">
+                              {{ selectedObject["mods.title.main"] }}
+                            </nuxt-link>
+                          </h3>
+
+                          <div class="abstract">
+                            <div v-if="selectedObject['mods.abstract']?.length>0">
+                              {{ trimString(selectedObject["mods.abstract"][0]) }}
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </template>
 
-            </div>
-          </div>
-
-
+            </SovietSurvivorsMapWithMetadata>
+          </client-only>
         </div>
       </div>
 
@@ -66,7 +75,7 @@ const selectedObject = ref(null as any);
 
 if (path === '/soviet-survivors/') {
   const {data, error} = await useAsyncData('solr-response', () => {
-    return $fetch($sovietSurvivorsSolrURL() + 'mir/select?q=%2BobjectType%3A"mods"&fl=*&sort=mods.dateIssued+desc&rows=99999&fl=common.mods.coordinates:*&wt=json&XSL.Style=xml');
+    return $fetch($sovietSurvivorsSolrURL() + 'mir/select?q=%2BobjectType%3A"mods"&fl=*&sort=mods.dateIssued+desc&rows=99999&fq=common.mods.coordinates.str:*&fl=common.mods.coordinates:*&wt=json&XSL.Style=xml');
   });
 
   if (error.value) {
@@ -97,6 +106,36 @@ const hitLink = (doc: any) => {
 
 </script>
 <style scoped>
+.documents_card {
+  overflow: scroll;
+  height: 500px !important;
+}
+
+.document:not(:last-child) {
+  border-bottom: 1px solid;
+  border-image: linear-gradient(to right, transparent, #006978, transparent) 1;
+  padding-bottom: 2rem;
+  margin-bottom: 2rem;
+}
+
+.abstract {
+  text-align: justify;
+  hyphens: auto;
+  -webkit-hyphens: auto;
+  -ms-hyphens: auto;
+  word-break: break-word;
+  /* Optional: Mindestbreite für Trennung */
+  hyphenate-limit-chars: 6 3 3;
+  /* Optional: Sprache für korrekte Trennung */
+
+}
 
 
+</style>
+
+<style>
+.ol-overlay-container {
+  width: 30% !important;
+
+}
 </style>
