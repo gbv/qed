5457412e591084cffa55cc2269e50fa676ac5e7f
Tilo Neumann <tilo.neumann@gbv.de>
2026-01-30 19:48:23 +0100

    QUED-188 updates page structure and given content

--- edition-archive-frontend/components/LoD/Menu.vue
+++ edition-archive-frontend/components/LoD/Menu.vue
@@ -6,18 +6,16 @@
         <li class="nav-item">
           <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project">
             {{ $t('lod.subMenu.intro') }}
-
           </nuxt-link>
         </li>
         <li class="nav-item">
           <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/team">
             {{ $t('lod.subMenu.team') }}
-
           </nuxt-link>
         </li>
         <li class="nav-item">
-          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/partner">
-            {{ $t('lod.subMenu.projectPartner') }}
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/acknowledgments">
+            {{ $t('lod.subMenu.acknowledgments') }}
           </nuxt-link>
         </li>
         <li class="nav-item">
@@ -26,13 +24,75 @@
           </nuxt-link>
         </li>
         <li class="nav-item">
-          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/citation">
-            {{ $t('lod.subMenu.citation') }}
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/export">
+            {{ $t('lod.subMenu.export') }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </template>
+
+  <template v-if="$route.params.guidelinesPath =='' || $route.params.guidelinesPath">
+    <nav class="navbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines">
+            {{ $t('lod.subMenu.types') }}
           </nuxt-link>
         </li>
         <li class="nav-item">
-          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/project/export">
-            {{ $t('lod.subMenu.export') }}
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/descriptions">
+            {{ $t('lod.subMenu.descriptions') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/metadata">
+            {{ $t('lod.subMenu.metadata') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/titles">
+            {{ $t('lod.subMenu.titles') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/names">
+            {{ $t('lod.subMenu.names') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/dates">
+            {{ $t('lod.subMenu.dates') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/principles">
+            {{ $t('lod.subMenu.principles') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/copies">
+            {{ $t('lod.subMenu.copies') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/marking">
+            {{ $t('lod.subMenu.marking') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/commentary">
+            {{ $t('lod.subMenu.commentary') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/references">
+            {{ $t('lod.subMenu.references') }}
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" href="/languages-of-diplomacy/guidelines/tei">
+            {{ $t('lod.subMenu.tei') }}
           </nuxt-link>
         </li>
       </ul>
---
+++ edition-archive-frontend/pages/languages-of-diplomacy/guidelines/[[guidelinesPath]].vue
@@ -0,0 +1,20 @@
+<template>
+  <LoDLayout :headline="$t('lod.mainMenu.guidelines')">
+
+    <template #content>
+      <CMSPage default-language="en" :languages="['de', 'en']" :slug="`/qed${path}`"/>
+    </template>
+
+    <template #menu>
+      <LoDMenu/>
+    </template>
+
+  </LoDLayout>
+</template>
+
+<script setup lang="ts">
+  const {path} = useRoute();
+</script>
+
+<style scoped>
+</style>
