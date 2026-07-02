bca465924aae88e11e7724802f8ce40382220f80
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2026-07-02 09:45:59 +0200

    fix: add uk to about section

--- edition-archive-frontend/pages/soviet-survivors/about/[[aboutPath]].vue
+++ edition-archive-frontend/pages/soviet-survivors/about/[[aboutPath]].vue
@@ -2,7 +2,7 @@
   <SovietSurvivorsLayout :headline="$t('lod.mainMenu.about')">
 
     <template #content>
-      <CMSPage default-language="en" :languages="['de', 'en', 'ru']" :slug="`/qed${path}`"/>
+      <CMSPage default-language="en" :languages="['de', 'en', 'uk', 'ru']" :slug="`/qed${path}`"/>
     </template>
 
     <template #menu>
